7b9bc4165f2d53b663b5f14a92795b2d3891ca67
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2022-04-19 01:04:12 +0200

    1.18.2

--- src/main/java/me/voidxwalker/autoreset/mixin/MoreOptionsDialogMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/MoreOptionsDialogMixin.java
@@ -2,6 +2,7 @@ package me.voidxwalker.autoreset.mixin;
 
 import me.voidxwalker.autoreset.Atum;
 import net.minecraft.client.gui.screen.world.MoreOptionsDialog;
+import net.minecraft.world.gen.GeneratorOptions;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,27 +14,14 @@ import java.util.OptionalLong;
 
 @Mixin(MoreOptionsDialog.class)
 public abstract class MoreOptionsDialogMixin {
-    @Shadow private static OptionalLong tryParseLong(String string) {
-        return OptionalLong.empty();
-    }
+
 
     @ModifyArg(method = "getGeneratorOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/GeneratorOptions;withHardcore(ZLjava/util/OptionalLong;)Lnet/minecraft/world/gen/GeneratorOptions;",ordinal = 0),index = 1)
     public OptionalLong setSeed(OptionalLong originalSeed){
         if(!Atum.isRunning){
             return originalSeed;
         }
-        String string = Atum.seed==null?"": Atum.seed;
-        OptionalLong optionalLong;
-        if (StringUtils.isEmpty(string)) {
-            optionalLong = OptionalLong.empty();
-        } else {
-            OptionalLong optionalLong2 = tryParseLong(string);
-            if (optionalLong2.isPresent() && optionalLong2.getAsLong() != 0L) {
-                optionalLong = optionalLong2;
-            } else {
-                optionalLong = OptionalLong.of(string.hashCode());
-            }
-        }
+        OptionalLong optionalLong = GeneratorOptions.parseSeed(Atum.seed);
         Atum.log(Level.INFO,"Resetting "+((Atum.seed ==null|| Atum.seed.isEmpty()?"a random seed":"the set seed: "+"\""+optionalLong.getAsLong()+"\"")));
         return optionalLong;
     }
