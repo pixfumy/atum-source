a3cf0edbd9814546ba760b0a99f25a166200db52
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2022-06-30 19:13:21 +0200

    1.1.0

--- src/main/java/me/voidxwalker/autoreset/Atum.java
+++ src/main/java/me/voidxwalker/autoreset/Atum.java
@@ -17,8 +17,7 @@ import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
 import java.io.*;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
 
 public class Atum implements ModInitializer {
     public static boolean isRunning = false;
@@ -30,6 +29,7 @@ public class Atum implements ModInitializer {
     public static int attempts;
     public static boolean structures=true;
     public static boolean bonusChest=false;
+    static Map<String,String> extraProperties= new LinkedHashMap<>();
 
     static File configFile;
 
@@ -65,7 +65,9 @@ public class Atum implements ModInitializer {
                 GLFW.GLFW_KEY_P,
                 getTranslation("key.categories.atum","Atum").getString()
         ));
-        configFile = new File("config/atum.properties");
+        new File("config/atum").mkdir();
+        configFile = new File("config/atum/atum.properties");
+
         if(!configFile.exists()){
             try {
                 configFile.createNewFile();
@@ -137,6 +139,7 @@ public class Atum implements ModInitializer {
             properties.put("generatorType", String.valueOf(generatorType));
             properties.put("structures",String.valueOf(structures));
             properties.put("bonusChest",String.valueOf(bonusChest));
+            properties.putAll(extraProperties);
 
             properties.store(f,"This is the config file for Atum.\nseed: leave empty for a random seed\ndifficulty: -1 = HARDCORE, 0 = PEACEFUL, 1 = EASY, 2= NORMAL, 3= HARD \ngeneratorType: 0 = DEFAULT, 1= FLAT, 2= LARGE_BIOMES, 3 = AMPLIFIED, 4 = SINGLE_BIOME_SURFACE, 5 = SINGLE_BIOME_CAVES, 6 =SINGLE_BIOME_FLOATING_ISLANDS");
         } catch (IOException e) {
@@ -144,34 +147,42 @@ public class Atum implements ModInitializer {
         }
     }
     static void loadFromProperties(Properties properties){
-        seed =properties==null||!properties.containsKey("seed")?"": properties.getProperty("seed");
-        if(seed==null){
-            seed="";
+        if(properties!=null){
+            for (Map.Entry<Object,Object> entry: properties.entrySet()) {
+                if(!entry.getKey().equals("seed")&&!entry.getKey().equals("difficulty")&&!entry.getKey().equals("generatorType")&&!entry.getKey().equals("attempts")&&!entry.getKey().equals("structures")&&!entry.getKey().equals("bonusChest")){
+                    extraProperties.put((String)entry.getKey(),(String)entry.getValue());
+                }
+            }
+            seed = !properties.containsKey("seed") ?"": properties.getProperty("seed");
+            if(seed==null){
+                seed="";
+            }
+            seed=seed.trim();
+            try{
+                difficulty = !properties.containsKey("difficulty") ?1:Integer.parseInt(properties.getProperty("difficulty"));
+            } catch (NumberFormatException e) {
+                difficulty=1;
+            }
+            if(difficulty > 4){
+                difficulty = 1;
+            }
+            try{
+                generatorType =properties==null||!properties.containsKey("generatorType")?0:Integer.parseInt(properties.getProperty("generatorType"));
+            } catch (NumberFormatException e) {
+                generatorType=0;
+            }
+            if(generatorType > 6){
+                generatorType = 0;
+            }
+            try{
+                attempts =properties==null||!properties.containsKey("attempts")?0:Integer.parseInt(properties.getProperty("attempts"));
+            } catch (NumberFormatException e) {
+                attempts=0;
+            }
+            structures = properties == null || !properties.containsKey("structures") || Boolean.parseBoolean(properties.getProperty("structures"));
+            bonusChest = properties != null &&  Boolean.parseBoolean(properties.getProperty("bonusChest"));
         }
-        seed=seed.trim();
-        try{
-            difficulty =properties==null||!properties.containsKey("difficulty")?1:Integer.parseInt(properties.getProperty("difficulty"));
-        } catch (NumberFormatException e) {
-            difficulty=1;
-        }
-        if(difficulty > 4){
-            difficulty = 1;
-        }
-        try{
-            generatorType =properties==null||!properties.containsKey("generatorType")?0:Integer.parseInt(properties.getProperty("generatorType"));
-        } catch (NumberFormatException e) {
-            generatorType=0;
-        }
-        if(generatorType > 6){
-            generatorType = 0;
-        }
-        try{
-            attempts =properties==null||!properties.containsKey("attempts")?0:Integer.parseInt(properties.getProperty("attempts"));
-        } catch (NumberFormatException e) {
-            attempts=0;
-        }
-        structures = properties == null || !properties.containsKey("structures") || Boolean.parseBoolean(properties.getProperty("structures"));
-        bonusChest = properties != null &&  Boolean.parseBoolean(properties.getProperty("bonusChest"));
+
     }
     public enum HotkeyState{
         OUTSIDE_WORLD,
