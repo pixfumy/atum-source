56eed4f790d93eacb02acf5fadc752944a7cd015
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2022-07-12 19:56:57 +0200

    1.18.2 port

---
+++ src/main/java/me/voidxwalker/autoreset/mixin/hotkey/GameOptionsMixin.java
@@ -0,0 +1,25 @@
+package me.voidxwalker.autoreset.mixin.hotkey;
+
+import me.voidxwalker.autoreset.KeyBindingHelper;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameOptions.class)
+public class GameOptionsMixin {
+
+
+    @Mutable
+    @Shadow @Final public KeyBinding[] allKeys;
+
+    @Inject(at = @At("HEAD"), method = "load()V")
+    public void loadHook(CallbackInfo info) {
+        allKeys = KeyBindingHelper.process(allKeys);
+    }
+}
