af44d9728a67b9792753452b5fa84b7b367e5f9f
luna <voidxwalker77@gmail.com>
2022-07-31 18:36:23 +0200

    1.1.2

--- src/main/java/me/voidxwalker/autoreset/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/CreateWorldScreenMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.IOException;
 import java.util.Random;
 
 
@@ -67,7 +68,11 @@ public abstract class CreateWorldScreenMixin {
         setGeneratorType(Atum.generatorType);
         setGenerateStructure(Atum.structures);
         setGenerateBonusChest(Atum.bonusChest);
-        Atum.saveProperties();
+        try {
+            Atum.saveProperties();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         levelNameField.setText((Atum.seed==null|| Atum.seed.isEmpty())?"Random Speedrun #" + Atum.rsgAttempts:"Set Speedrun #" + Atum.ssgAttempts);
         return ""+l;
     }
