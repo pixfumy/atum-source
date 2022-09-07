b3af15cdae52879e8f221a2e2fc5b3f977d2e268
luna <voidxwalker77@gmail.com>
2022-09-07 23:17:01 +0200

    1.1.2

--- src/main/java/me/voidxwalker/autoreset/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/CreateWorldScreenMixin.java
@@ -94,7 +94,7 @@ public abstract class CreateWorldScreenMixin extends Screen{
         Atum.saveProperties();
         Atum.log(Level.INFO,(Atum.seed==null|| Atum.seed.isEmpty()?"Resetting a random seed":"Resetting the set seed"+"\""+l+"\""));
         levelNameField.setText((Atum.seed==null|| Atum.seed.isEmpty())?"Random Speedrun #" + Atum.rsgAttempts:"Set Speedrun #" + Atum.ssgAttempts);
-        this.client.startGame(levelNameField.getText(), levelNameField.getText(), levelInfo);
+        this.client.startGame(this.saveDirectoryName, levelNameField.getText().trim(), levelInfo);
 
 
 
